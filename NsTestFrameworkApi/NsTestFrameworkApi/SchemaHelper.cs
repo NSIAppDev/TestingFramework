a18789f15c320ee138820e36cdd709732a51fc43
Ramona Zmole <ramona.zmole@gmail.com>
2021-03-24 15:36:55 +0200

    add SchemaHelper

---
+++ NsTestFrameworkApi/NsTestFrameworkApi/SchemaHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
+
+namespace NsTestFrameworkApi
+{
+    public static class SchemaHelper
+    {
+        public static bool IsSchemaValid(this string response, Type objectType)
+        {
+            var responseToken = JToken.Parse(response);
+            var generatedSchema = new JSchemaGenerator { ContractResolver = new CamelCasePropertyNamesContractResolver() }.Generate(objectType);
+            return responseToken.IsValid(generatedSchema, out IList<string> _);
+        }
+    }
+}
