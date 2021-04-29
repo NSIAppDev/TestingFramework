ad1ac5f095c1106a66f6ba81106f35836f73e107
Ramona Zmole <ramona.zmole@gmail.com>
2021-04-29 10:16:16 +0300

    change ScreenShot method

--- NsTestFrameworkUI/NsTestFrameworkUI/Helpers/ScreenShot.cs
+++ NsTestFrameworkUI/NsTestFrameworkUI/Helpers/ScreenShot.cs
@@ -11,16 +11,22 @@ namespace NsTestFrameworkUI.Helpers
             return dateTime.ToString("yyyyMMddHHmmss");
         }
 
-        public static void TakeAndAttachScreenShot(string testName)
+        public static string GetScreenShotPath(string testName)
         {
             var screenShot = ((ITakesScreenshot)Browser.WebDriver).GetScreenshot();
 
             var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots");
-            var imageName = $"{testName}_{GetTimestamp(DateTime.Now)}.png";
-            var imagePath = Path.Combine(folderPath, imageName);
+            var imagePath = GetImagePath(folderPath, testName);
 
             Directory.CreateDirectory(folderPath);
             screenShot.SaveAsFile(imagePath);
+            return imagePath;
+        }
+
+        private static string GetImagePath(string folderPath, string testName)
+        {
+            var imageName = $"{testName}_{GetTimestamp(DateTime.Now)}.png";
+            return Path.Combine(folderPath, imageName);
         }
     }
 }
