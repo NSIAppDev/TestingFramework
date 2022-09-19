92ab798455993f907ed353939cfa77e8ab3a21b0
Ramona Zmole <rzmole@northernsafety.com>
2022-09-19 18:23:51 +0300

    add port helper

---
+++ NsTestFrameworkApi/NsTestFrameworkApi/PortHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace NsTestFrameworkApi;
+
+internal static class PortHelper
+{
+    internal static (int Port1, int Port2) Ports { get; } = GetAvailablePorts();
+
+    private static (int Port1, int Port2) GetAvailablePorts()
+    {
+        var ip = Dns.GetHostEntry("localhost").AddressList[0];
+
+        TcpListener listener1 = new(ip, 0);
+        listener1.Start();
+        var port1 = ((IPEndPoint)listener1.LocalEndpoint).Port;
+
+        TcpListener listener2 = new(ip, 0);
+        listener2.Start();
+        var port2 = ((IPEndPoint)listener2.LocalEndpoint).Port;
+
+        listener1.Stop();
+        listener2.Stop();
+
+        return (port1, port2);
+    }
+
+    internal static string BuildUrlUsingAvailablePort(string host, int port)
+        => $"{host}:{port}/";
+}
